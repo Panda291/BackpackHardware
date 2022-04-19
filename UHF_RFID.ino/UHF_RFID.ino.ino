85188a478a2f0d53d249904e4e93d8c93414df75
panad-laptop <christophevandeneede@hotmail.com>
2022-04-19 20:03:00 +0200

    fixed no tags found issue

--- UHF_RFID.ino/UHF_RFID.ino.ino
+++ UHF_RFID.ino/UHF_RFID.ino.ino
@@ -6,8 +6,8 @@
 
 UHF_RFID RFID;
 
-const char* ssid = "Casa El Escrino";
-const char* password = "HaFSngs6JU";
+const char* ssid = "Van den Eede";
+const char* password = "a123456789";
 
 String comd = " ";
 CardpropertiesInfo card;
@@ -52,33 +52,15 @@ void loop()
   M5.Lcd.fillCircle(310, 10, 6, BLACK);
   RFID.Delay(150);
 
-//  comd = RFID.Set_the_select_parameter_directive("30751FEB705C5904E3D50D70");
-//  M5.Lcd.drawString(comd, 0, 0, 2);
-//  RFID.Delay(1000);
-//  RFID.clean_data();
-//  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-//
-//  card = RFID.A_single_poll_of_instructions();
-//  if (card._ERROR.length() != 0)
-//  {
-//    Serial.print(card._ERROR);
-//  }
-//  else
-//  {
-//    Serial.print(card._RSSI);
-//    M5.Lcd.drawString(card._RSSI, 0, 0, 2);
-//    M5.Lcd.drawString(card._PC, 0, 15, 2);
-//    M5.Lcd.drawString(card._EPC, 0, 30, 2);
-//    M5.Lcd.drawString(card._CRC, 0, 45, 2);
-//  }
-//  RFID.Delay(1000);
-//  RFID.clean_data();
-//  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-//
-//RFID.Delay(1000);
 if (!digitalRead(HALL)) {
   RFIDs = "RFIDs=[";
-  cards = RFID.Multiple_polling_instructions(10);
+  int counter = 0;
+  while ((cards = RFID.Multiple_polling_instructions(10)).len == 0 && counter < 5) {
+      RFID.Delay(300);
+      counter++;
+  }
+
+  if (cards.len > 0) {
   for (size_t i = 0; i < cards.len; i++)
   {
     RFIDs += "\"" + cards.card[i]._EPC + "\",";
@@ -92,7 +74,24 @@ if (!digitalRead(HALL)) {
   M5.Lcd.printf("%s\n", RFIDs.c_str());
   RFID.Delay(1000);
   RFID.clean_data();
-  if(WiFi.status() == WL_CONNECTED) {
+  
+  httpRequest();
+  }
+
+  //  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
+      while (!digitalRead(HALL)) {
+        M5.Lcd.fillCircle(310, 10, 6, RED);
+        RFID.Delay(150);
+        M5.Lcd.fillCircle(310, 10, 6, BLACK);
+        RFID.Delay(150);
+      }
+      M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
+
+}
+}
+
+void httpRequest() {
+if(WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
 
     http.begin("http://backpack.cvdeede.be/api/set_contents?" + RFIDs);
@@ -113,82 +112,4 @@ if (!digitalRead(HALL)) {
 
     http.end();
   }
-//  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-    while (!digitalRead(HALL)) {
-      M5.Lcd.fillCircle(310, 10, 6, RED);
-      RFID.Delay(150);
-      M5.Lcd.fillCircle(310, 10, 6, BLACK);
-      RFID.Delay(150);
-    }
-    M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-
-}
-//
-//  Select = RFID.Get_the_select_parameter();
-//  M5.Lcd.drawString(Select.Mask, 0, 0, 2);
-//  M5.Lcd.drawString(Select.SelParam, 0, 15, 2);
-//  M5.Lcd.drawString(Select.Ptr, 0, 30, 2);
-//  M5.Lcd.drawString(Select.MaskLen, 0, 45, 2);
-//  M5.Lcd.drawString(Select.Truncate, 0, 60, 2);
-//  RFID.Delay(1000);
-//  RFID.clean_data();
-//  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-//
-//  Cardinformation = RFID.NXP_Change_EAS(0x00000000);
-//  M5.Lcd.drawString(Cardinformation._UL, 0, 0, 2);
-//  M5.Lcd.drawString(Cardinformation._PC, 0, 15, 2);
-//  M5.Lcd.drawString(Cardinformation._EPC, 0, 30, 2);
-//  M5.Lcd.drawString(Cardinformation._Parameter, 0, 45, 2);
-//  M5.Lcd.drawString(Cardinformation._ErrorCode, 0, 60, 2);
-//  M5.Lcd.drawString(Cardinformation._Error, 0, 75, 2);
-//  M5.Lcd.drawString(Cardinformation._Data, 0, 90, 2);
-//  M5.Lcd.drawString(Cardinformation._Successful, 0, 105, 2);
-//  RFID.Delay(1000);
-//  RFID.clean_data();
-//  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-//
-//  Query = RFID.Get_the_Query_parameter();
-//  M5.Lcd.drawString(Query.QueryParameter, 0, 0, 2);
-//  M5.Lcd.drawString(Query.DR, 0, 15, 2);
-//  M5.Lcd.drawString(Query.M, 0, 30, 2);
-//  M5.Lcd.drawString(Query.TRext, 0, 45, 2);
-//  M5.Lcd.drawString(Query.Sel, 0, 60, 2);
-//  M5.Lcd.drawString(Query.Session, 0, 75, 2);
-//  M5.Lcd.drawString(Query.Target, 0, 90, 2);
-//  M5.Lcd.drawString(Query.Q, 0, 105, 2);
-//  RFID.Delay(1000);
-//  RFID.clean_data();
-//  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-//
-//
-//  Read = RFID.Read_receive_demodulator_parameters();
-//  M5.Lcd.drawString(Read.Region, 0, 0, 2);
-//  M5.Lcd.drawString(Read.Channel_Index, 0, 15, 2);
-//  M5.Lcd.drawString(Read.Pow, 0, 30, 2);
-//  M5.Lcd.drawString(Read.Mixer_G, 0, 45, 2);
-//  M5.Lcd.drawString(Read.IF_G, 0, 60, 2);
-//  M5.Lcd.drawString(Read.Thrd, 0, 75, 2);
-//  RFID.Delay(1000);
-//  RFID.clean_data();
-//  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-//
-//  Test = RFID.Test_the_RSSI_input_signal();
-//  M5.Lcd.drawString(Test.CH_L, 0, 0, 2);
-//  M5.Lcd.drawString(Test.CH_H, 0, 15, 2);
-//  for (size_t i = 0; i < 20; i++)
-//  {
-//    if (i < 10)
-//    {
-//      M5.Lcd.drawString(Test.Data[i], i * 20, 30, 2);
-//    }
-//    else
-//    {
-//      M5.Lcd.drawString(Test.Data[i], (i - 10) * 20, 45, 2);
-//    }
-//  }
-//  RFID.Delay(1000);
-//  RFID.clean_data();
-//  M5.Lcd.fillRect(0, 0, 340, 280, BLACK);
-
-
 }
